5874dc9af7a22da1e4433309dd39a573a28cbb05
John <davis.p.john@gmail.com>
2022-03-05 20:30:50 -0600

    Initial Commit

---
+++ Sources/MultipeerKit/MultipeerKit.swift
@@ -0,0 +1,6 @@
+public struct MultipeerKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MultipeerKitTests/MultipeerKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MultipeerKit
+
+final class MultipeerKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MultipeerKit().text, "Hello, World!")
+    }
+}
